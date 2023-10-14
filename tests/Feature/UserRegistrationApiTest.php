216eb56177fad50d456592c6b82db63a5e0c7c9d
UrikRS <610521224@gms.ndhu.edu.tw>
2023-10-14 08:24:46 +0000

    1

---
+++ tests/Feature/UserRegistrationApiTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UserRegistrationApiTest extends TestCase
+{
+    use WithFaker;
+    use RefreshDatabase;
+
+    public function testUserRegistration(){
+        $userData = [
+            'name' => $this->faker->name,
+            'phone' => $this->faker->e164PhoneNumber,
+            'email' => $this->faker->safeEmail,
+        ];
+
+        $response = $this->json('POST', '/users', $userData);
+        $response->assertStatus(200);
+        $response->assertJsonStructure([
+            'data' => [
+                'id',
+                'name',
+                'phone',
+                'email',
+            ],
+        ]);
+
+        // You can also assert more specific details about the user created, e.g., in the database
+        $this->assertDatabaseHas('users', [
+            'email' => $userData['email'],
+        ]);
+    }
+
+}
+
